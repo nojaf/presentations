7b0a5c4f8f2f2b57de26ea765f0fa22ce9fbc17f
nojaf <florian.verdonck@outlook.com>
2020-05-04 21:07:18 +0200

    case-study.fsx

---
+++ formatting-fsharp-code/case-study.fsx
@@ -0,0 +1,15 @@
+/// Formatting F# code
+/// A case study
+
+type FormatConfig = {
+    PageWidth: int
+    Indent: int } // the number of spaces
+
+let binaryUIntValue = 0x4000u
+
+type ILogger =
+    member this.LogDebug message = printfn (* TODO: implement something *) ()
+
+let run (req: HttpRequest) (log: ILogger) =
+    Http.main getFantomasVersion format FormatConfig.FormatConfig.Default log req
+    |> Async.StartAsTask
